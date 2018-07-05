5bf66c57ee8dd63055aa2fa686243eff4112e4bd
aimeelynnramirez <aimeelynnramirez@gmail.com>
2018-07-05 17:37:38 -0400

    added the final touches adding in package the start in both build and prod

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 // //
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
 // console.log(`I'm a silly entry point`);
-import App from "./App";
-
 const arr = [1, 2, 3];
 const iAmJavascriptES6 = () => console.log(...arr);
 window.iAmJavascriptES6 = iAmJavascriptES6;

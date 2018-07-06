41f64e07778531220f0df397bc85bbe2f9396ff0
aimeelynnramirez <aimeelynnramirez@gmail.com>
2018-07-05 21:02:20 -0400

    added static

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 // //
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App.test />, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('app'));
 registerServiceWorker();
 // console.log(`I'm a silly entry point`);
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,13 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader"
         }
       },
+
       {
         test: /\.html$/,
         use: [
